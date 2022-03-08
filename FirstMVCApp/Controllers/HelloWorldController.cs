bc29b65fc22b32563a21eb6707e83135e406f89e
JannickL <jannick.leismann@gmx.de>
2022-03-08 13:09:28 +0100

    Push project

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {              
+            return View(dogs);            
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ FirstMVCApp/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstMVCApp.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
